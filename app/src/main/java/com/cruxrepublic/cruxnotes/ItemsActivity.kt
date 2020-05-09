03bbf2202c336902144882f573114a894666be73
dancrux <nwogwugwudaniel@gmail.com>
2020-05-09 02:24:50 +0100

    Added more functionality to the navigation drawer (the courseAdapter for Course list)

--- app/src/main/java/com/cruxrepublic/cruxnotes/ItemsActivity.kt
+++ app/src/main/java/com/cruxrepublic/cruxnotes/ItemsActivity.kt
@@ -4,26 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.annotation.StringRes
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_item.*
 import kotlinx.android.synthetic.main.content_item.*
 
-class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+
+class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private val noteLayoutManager by lazy {
+        LinearLayoutManager(this)
+    }
+    private val noteRecyclerAdapter by lazy {
+        NoteRecyclerAdapter(this, DataManager.notes)
+    }
+    private val courseLayoutManager by lazy {
+        GridLayoutManager(this, 2)
+    }
+    private val courseRecyclerAdapter by lazy {
+        CourseRecyclerAdapter(this, DataManager.courses.values.toList())
+    }
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +48,7 @@ class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 //                .setAction("Action", null).show()
         }
 
-        listItems.layoutManager = LinearLayoutManager(this)
-        listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+        displayNotes()
 
         val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, 0, 0)
         drawer_layout.addDrawerListener(toggle)
@@ -55,11 +63,25 @@ class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_notes, R.id.nav_courses, R.id.nav_slideshow
             ), drawerLayout
         )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 //        navView.setupWithNavController(navController)
+    }
+
+    private fun displayNotes() {
+        listItems.layoutManager = noteLayoutManager
+        listItems.adapter = noteRecyclerAdapter
+
+        nav_view.menu.findItem(R.id.nav_notes).isChecked = true
+    }
+
+    private fun displayCourses(){
+        listItems.layoutManager = courseLayoutManager
+        listItems.adapter = courseRecyclerAdapter
+
+        nav_view.menu.findItem(R.id.nav_courses).isChecked = true
     }
 
     override fun onResume() {
@@ -82,7 +104,20 @@ class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return false
+        when (item.itemId) {
+            R.id.nav_notes -> {
+               displayNotes()
+            }
+            R.id.nav_courses -> {
+                displayCourses()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun handleSelection(message: String) {
+      Snackbar.make(listItems, message , Snackbar.LENGTH_LONG).show()
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
